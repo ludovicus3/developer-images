79739dafdf9b947a2a5d7598ac027d22e79508d2
Jason Galens <jason@galensfamily.org>
2024-01-05 09:05:23 -0500

    Some cleanup of bookworm

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Ensure $HOME exists when starting
+if [ ! -d "${HOME}" ]; then
+  mkdir -p "${HOME}"
+fi
+
+# Add current (arbitrary) user to /etc/passwd and /etc/group
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
+
 # Stow
 ## Required for https://github.com/eclipse/che/issues/22412
 
